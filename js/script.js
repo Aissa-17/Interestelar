9ba38b2397f395eb226e3e8e1ea4de17511e5d5e
aissa <aissa.allay@gmail.com>
2025-02-10 10:38:37 +0100

    Descripción de los cambios

--- js/script.js
+++ js/script.js
@@ -2,17 +2,3 @@ document.getElementById("contacto").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("¡Formulario enviado con éxito!");
 });
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdownContent");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-    }
-
-    // Cerrar el desplegable si se hace clic fuera
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropdown-toggle')) {
-            var dropdown = document.getElementById("dropdownContent");
-            if (dropdown.style.display === "block") {
-                dropdown.style.display = "none";
-            }
-        }
-    }
